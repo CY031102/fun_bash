b613e2cc4960615fb2f64d5dc031f2446b557da5
Zep <52792159+CY031102@users.noreply.github.com>
2024-01-30 10:22:26 -0800

    Create fun.sh

---
+++ fun.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "Hello,"
+echo "###System Will Reboot in 10s"
+echo "Ctrl+C to Cancel"
+echo -ne '#               (10%)\r'
+sleep 1
+echo -ne '##              (20%)\r'
+sleep 1
+echo -ne '###             (30%)\r'
+speep 1
+echo -ne '####            (40%)\r'
+sleep 1
+echo -ne '#####           (50%)\r'
+sleep 1
+echo -ne '######          (60%)\r'
+sleep1
+echo -ne '#######         (70%)\r'
+sleep 1
+echo -ne '########        (80%)\r'
+speep 1
+echo -ne '#########       (90%)\r'
+sleep 1
+echo -ne '##########      (100%)\r'
+sleep 1
+echo -ne '\n'
+echo '###System Rebooting"
+reboot
